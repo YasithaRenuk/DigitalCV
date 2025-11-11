1a0a3faacefc8b00b04d93a2a21dc3756aeec386
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2025-11-11 20:14:42 +0530

    Update page.tsx

--- src/app/(pages)/contactus/page.tsx
+++ src/app/(pages)/contactus/page.tsx
@@ -1,27 +1,32 @@
+export const dynamic = "force-dynamic";
 "use client";
 
+import { Suspense } from "react";
 import OurProcessCard from "../../components/ContactUS/OurProcessCard";
 import InquiryForm from "../../components/ContactUS/InquiryForm";
 import { useSearchParams } from "next/navigation";
 
-const ContactUS = () => {
+function ContactUSContent() {
   const searchParams = useSearchParams();
   const reasonTopic = searchParams.get("reasonTopic") || "";
 
   return (
     <div className="flex flex-col md:flex-row min-h-screen pt-10 md:pt-1 items-center justify-center">
-      {/* InquiryForm first on mobile, second on desktop */}
-      {/* { console.log(reasonTopic) } */}
       <div className="w-full md:order-2 md:w-[50%] flex items-center justify-center">
         <InquiryForm reasonTopic={reasonTopic} />
       </div>
 
-      {/* OurProcessCard second on mobile, first on desktop */}
       <div className="w-full md:order-1 md:w-[50%] flex items-center justify-center mt-8 md:mt-0">
         <OurProcessCard />
       </div>
     </div>
   );
-};
+}
 
-export default ContactUS;
+export default function ContactUS() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ContactUSContent />
+    </Suspense>
+  );
+}
