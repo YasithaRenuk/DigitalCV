a9662913dfedd415ea0e67995047a66cab0fe2c5
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2025-11-22 12:00:11 +0530

    Update LoginPage.tsx

--- src/app/components/LoginPage/LoginPage.tsx
+++ src/app/components/LoginPage/LoginPage.tsx
@@ -14,28 +14,28 @@ export default function LoginPage() {
         </p>
 
         <Button
-            onClick={() => signIn('google')}
-            variant="outline"
-            className="w-full border  text-gray-700 shadow-sm flex items-center justify-center gap-2 py-5 rounded-lg"
+          onClick={() => signIn("google")}
+          variant="outline"
+          className="w-full border mt-2.5 mb-2.5 h-12 text-gray-700 shadow-sm flex items-center justify-center gap-2 py-5 rounded-lg"
         >
-            <svg className="w-5 h-5" viewBox="0 0 24 24">
-                <path
-                  fill="currentColor"
-                  d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
-                />
-            </svg>
+          <svg className="w-16 h-16" viewBox="0 0 48 48">
+            <path
+              fill="#EA4335"
+              d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.67 13.21 17.91 9.5 24 9.5z"
+            />
+            <path
+              fill="#4285F4"
+              d="M46.1 24.55c0-1.57-.14-3.09-.39-4.55H24v9.02h12.55c-.54 2.8-2.13 5.16-4.52 6.79l7.02 5.44C43.82 37.24 46.1 31.34 46.1 24.55z"
+            />
+            <path
+              fill="#FBBC05"
+              d="M10.54 28.41c-.48-1.41-.75-2.92-.75-4.41s.27-3 .75-4.41L2.56 13.22C.91 16.59 0 20.2 0 24s.91 7.41 2.56 10.78l7.98-6.37z"
+            />
+            <path
+              fill="#34A853"
+              d="M24 48c6.48 0 11.93-2.13 15.9-5.77l-7.02-5.44c-2.04 1.35-4.65 2.16-8.88 2.16-6.09 0-11.33-3.71-13.46-9.08l-7.98 6.37C6.51 42.62 14.62 48 24 48z"
+            />
+          </svg>
           Continue with Google
         </Button>
 
