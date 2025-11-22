275351055b4182601845ef883233b0865e8ce69a
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2025-11-22 11:46:48 +0530

    Update page.tsx

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,15 +1,35 @@
 'use client';
 
+import { Button } from "@/components/ui/button";
 import FeatureSection from "../components/HomePage/FeatureSection";
 import Hero from "../components/HomePage/Hero";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 
 export default function Home() {
-
+  const { data: session } = useSession();
+  const router = useRouter();
+  
+  const onClickCreate = () => {
+    if (session) {
+      router.push("/createcv");
+    } else {
+      router.push("/loginpage");
+    }
+  };
   return (
     <div>
       <Hero/>
       <FeatureSection/>
+      <div className="w-full flex justify-center">
+        <div className="shadow-2xl w-[1183px] h-[383px] m-10 bg-white rounded-lg justify-center flex">
+          <Button className="mt-64" onClick={onClickCreate}>
+            Create Your Resume Now!
+          </Button>
+        </div>
+      </div>
+
     </div>
   );
 }
