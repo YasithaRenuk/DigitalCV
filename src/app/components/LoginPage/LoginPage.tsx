bca49a743ac4ffde03803be43669ec2ad566b738
YasithaRenuk <126389901+YasithaRenuk@users.noreply.github.com>
2025-12-11 18:20:37 +0530

    Update LoginPage.tsx

--- src/app/components/LoginPage/LoginPage.tsx
+++ src/app/components/LoginPage/LoginPage.tsx
@@ -5,7 +5,7 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function LoginPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen px-4 text-center">
+    <div className="flex flex-col items-center justify-center  px-4 text-center">
       <div className="max-w-md w-full space-y-6">
         <h1 className="text-2xl font-semibold">Login To Your Account</h1>
         <p className="text-gray-500 text-sm">
